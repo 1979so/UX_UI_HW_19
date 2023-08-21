659ed18e553c1d9128d758912d5cdcfa4b02b3ee
1979so <starla.olson.mft@gmail.com>
2023-08-21 14:31:03 -0600

    update

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,18 @@
 console.log("Your index.js file is loaded correctly!");
 
-// $("logo").click(function(){
+// $("logo-home-gif").click(function(){
 //     $(this).fadeToggle("slow", 0.5);
 // })
 
-// $("logo").on("mouseenter", function(){
+// $("Logo").on("mouseenter", function(){
 //     $(this).fadeIn("slow", 0.5); // Fade in on mouse enter
 // });
 
-// $("logo").on("mouseleave", function(){
+// $("Logo").on("mouseleave", function(){
 //     $(this).fadeOut("slow", 1);   // Fade out on mouse leave
 // });
 
-// $("logo").hover(
+// $("Logo").hover(
 //     function() {
 //       $(this).fadeIn("fast"); // Fade in on mouse enter
 //     },
@@ -21,10 +21,12 @@ console.log("Your index.js file is loaded correctly!");
 //     }
 //   );
 
+
 $(document).ready(function(){
-    $("logo").hover(function(){
-      $(this).fadeTo("fast", 0.5);
-    }, function(){
-      $(this).fadeTo("fast", 1);
-    });
+  $("Logo").hover(function(){
+    $(this).fadeTo("fast", 0.5);
+  }, function(){
+    $(this).fadeTo("fast", 1);
   });
+});
+  
